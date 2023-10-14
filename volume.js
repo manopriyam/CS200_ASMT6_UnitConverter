c3d89f05b31935baf052dc37f65f47995226f4f8
bhoomigoyal <bhoomig@iitbhilai.ac.in>
2023-10-14 13:23:34 +0530

    commit volume files

---
+++ volume.js
@@ -0,0 +1,49 @@
+let CubicMeter = document.getElementById("CubicMeter");
+let CubicCentimeter = document.getElementById("CubicCentimeter");
+let Liter = document.getElementById("Liter");
+let Milliliter = document.getElementById("Milliliter");
+
+function CubicMetertoOthers (val){
+    CubicCentimeter.value = val*1000000; 
+    Liter.value = val*1000; 
+    Milliliter.value= val*(1000000);
+}
+
+function LitertoOthers (val){
+    CubicCentimeter.value = val*1000; 
+	CubicMeter.value = val*0.001; 
+    Milliliter.value=val*1000;
+}
+
+function CubicCentimetertoOthers (val){
+    CubicMeter.value = val*0.000001; 
+    Liter.value = val*0.001; 
+    Milliliter.value= val*1;
+}
+    
+  function MillilitertoOthers (val){
+    CubicCentimeter.value = val*1; 
+    CubicMeter.value = val*0.000001; 
+    Liter.value= val*0.001;
+ }
+
+function convertToOthers(convertFrom,value){    
+    switch(convertFrom){
+        case "CubicMeter" : 
+            CubicMetertoOthers (parseFloat(value)); 
+            break;
+        case "CubicCentimeter" : 
+            CubicCentimetertoOthers (parseFloat(value)); 
+            break;
+        case "Liter" : 
+            LitertoOthers (parseFloat(value)); 
+            break;
+        case "Milliliter" : 
+            MillilitertoOthers (parseFloat(value)); 
+            break;
+            
+         }
+      }
+         
+         
+         
